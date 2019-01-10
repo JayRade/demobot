f73a16ac9958a3cf63c77c914eb02af159a65b29
Jade Rayner <jray3697@SHARED.SYDNEY.EDU.AU>
2019-01-10 16:03:33 +1100

    I made a demobot that I think works

---
+++ .vscode/demobot.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/weather')
+def weather():
+    weather = request.values.get('text')
+    if int(weather) > 30:
+        return "it's so hot!"
+    else:
+        return f'the temperature is {weather}.'
+
+
+@app.route('/greet')
+def greet_person():
+    name = request.values.get('text')
+    return f'hi {name}!'
+
+
+@app.route('/')
+def another_word():
+    return 'Another word'
+
+
+@app.route('/ncss')
+def ncss_webpage():
+    return '<h1> NCSS </h1>'
+
+if __name__ == '__main__':
+    app.run()
